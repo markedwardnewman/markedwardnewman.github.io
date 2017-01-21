e06f3b60297a899caaa76354000c3ee3ff4b15ca
Mark Newman <markedwardnewman@gmail.com>
2017-01-20 21:38:15 -0600

    Completely revamped: no frameworks or class names used

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,43 @@
-//Gruntfile.js for ultrapolite.com
+//Gruntfile.js for markedwardnewman.com
 
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-    
-    //SASS to CSS compiler
+
+    //CSS: SASS to CSS compiler
 		sass: {
 			dist: {
-        options: {
-          style: 'compressed',
-          sourcemap: 'none'
-        },
 				files: {
 					'assets/css/main.css' : 'src/assets/css/main.scss'
 				}
 			}
 		},
-    
-    //A generalized task with specialized components
+
+    //FILE: combines content source files into one file
+    concat: {
+      css: {
+        //Concatenate all of the files in the cssResources configuration property
+        src: [ 'src/assets/css/vendor/jquery-ui/jquery-ui.css','src/assets/css/main.css' ],
+        dest: 'assets/css/bundled.css',
+        options: {
+          separator: '\n\n\n\n\n\n\n\n\n\n/*concatenated from new sourcefile. see Grunfile.js*/\n'
+        }
+      },
+
+      js: {
+        src: ['src/assets/js/vendor/jquery/jquery.js',
+              'src/assets/js/vendor/jquery-ui/jquery-ui.js',
+              'src/assets/js/vendor/parallax/parallax.js',
+              'src/assets/js/src.main.js'],
+        dest: 'src/assets/js/bundled.js',
+        options: {
+          separator: '\n\n\n\n\n\n\n\n\n\n/*concatenated from new sourcefile. see Grunfile.js*/\n'
+        }
+      }
+    },
+
+
+    //CSS: A generalized task with specialized components
     postcss: {
       options: {
         processors: [
@@ -30,39 +50,41 @@ module.exports = function(grunt) {
 
           //Adds hexadecimal fallback colors when rgba colors are specified 
           require('postcss-color-rgba-fallback')(),
-          
+
           //Minify css       
           require('cssnano')()
         ]
       },
       dist: {
-        src: 'assets/css/main.css'
+        src: 'assets/css/bundled.css'
       }
     },
-    
-    //Removes unused CSS
-    uncss: { 
-      dist: {
+
+
+    //JS: mangles, minifies, and concatenates all js files into one file
+    uglify: {
+      options: {
+        mangle: true, //this does alot, but mainly it replaces long variable names with single letters when possible.
+        beautify: false, //turn this on and mangle off to see full source code
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+                '/*! mangled, minified, and concatenated using grunt-contrib-uglify */\n\n'
+      },
+      my_target: {
         files: {
-          'assets/css/main.css':'index.html'
+          //dest file:
+          'assets/js/bundled.js':
+
+          //src files:
+          [  'src/assets/js/vendor/jquery/jquery.js',
+             'src/assets/js/vendor/jquery-ui/jquery-ui.js',
+             'src/assets/js/vendor/parallax/parallax.js',
+             'src/assets/js/src.main.js']
         }
       }
     },
+
     
-    //Performs specified tasks based upon file changes
-		watch: {
-      css: {
-        files: 'src/assets/css/main.scss',
-        tasks: ['sass']
-        //tasks: ['sass','postcss']
-      }/* ,
-      html: {
-        files: 'index.html',
-        tasks: ['validation']
-      } */
-		},
-    
-    //Reports w3c validation errors
+    //HTML: Reports w3c validation errors
     validation: { 
       options: {
           reset: grunt.option('reset') || true,
@@ -77,16 +99,31 @@ module.exports = function(grunt) {
       files: {
           src: 'index.html'
       }
-    }
+    },
+
+
+    //WATCH: Performs specified tasks based upon file changes
+		watch: {
+      css: {
+        files: 'src/assets/css/main.scss',
+        tasks: ['sass']
+        //tasks: ['sass','postcss']
+      }/* ,
+      html: {
+        files: 'index.html',
+        tasks: ['validation']
+      } */
+		}
 	});
   
+  grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks('grunt-uncss');;  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-w3c-html-validation');
+
 	grunt.registerTask('default',['watch']);
-  grunt.registerTask('test',['validation']);
-  //grunt.registerTask('all',['sass','postcss','uncss','validation']);
-  grunt.registerTask('all',['sass','postcss','validation']);
+  //grunt.registerTask('all',['sass','postcss','validation']);
+  grunt.registerTask('test1',['sass','concat','postcss','uglify']);
 }
