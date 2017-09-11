0b844908e2e80d25d03dff6e76d464dfb5f6d6a5
Mark Newman <markedwardnewman@gmail.com>
2017-09-10 23:27:39 -0500

    Media query tweak

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    var sliders = document.getElementsByTagName('aside');
-    var accordion = document.querySelectorAll('#about h3, #skills h3');
+    //var sliders = document.getElementsByTagName('aside');
     //var hidden_anchors = document.querySelectorAll('#about a, #skills a')
-    window.addEventListener('scroll', slide(sliders));
-	
+    //window.addEventListener('scroll', slide(sliders));
+    
+    var accordion = document.querySelectorAll('#about h3, #skills h3');
     //Accordion
     (function() {
         for (var i = 0; i < accordion.length; i++) {
@@ -12,20 +12,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
         }
     })();
 });
-
-function slide(el) {
-    for (var i = 0; i < el.length; i++) {
-        if (isVisible(el[i]) == false) {
-            el[i].classList.toggle('figure');
-        }
-    }
-}
-
-function isVisible(el) {
-    var rect = el.getBoundingClientRect();
-    var html = document.documentElement;
-    return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || html.clientHeight) && rect.right <= (window.innerWidth || html.clientWidth));
-}
 
 function clickAccordion() {
     this.classList.toggle('active');
@@ -49,3 +35,17 @@ function enterAccordion(event) {
         }
     }
 }
+
+/* function slide(el) {
+    for (var i = 0; i < el.length; i++) {
+        if (isVisible(el[i]) == false) {
+            el[i].classList.toggle('figure');
+        }
+    }
+} */
+
+/* function isVisible(el) {
+    var rect = el.getBoundingClientRect();
+    var html = document.documentElement;
+    return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || html.clientHeight) && rect.right <= (window.innerWidth || html.clientWidth));
+} */
